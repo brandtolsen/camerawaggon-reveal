752c0abe70c656ffb99326c6713bcd23f5f45310
Daniel Brandt-Olsen <daniel@brandtolsen.com>
2020-11-26 14:39:13 +0100

    pin element

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
+gsap.registerPlugin(ScrollTrigger);
+
 function init(){
     
     // move part 3 to cover part 2
     gsap.set('.part3', {
         y: () => {
             return -(document.querySelector('.pen-top').clientHeight - 22)
+        },
+        scrollTrigger: {
+            id: 'pen-body',
+            trigger: '.part3',
+            start: 'top bottom-=270px',
+            end: `+=${document.querySelector('.pen-top').clientHeight - 22}`,
+            pin: true,
+            markers: true
         }
-    })
+    });
 
 }
 
